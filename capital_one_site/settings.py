dc794dd67b0702b26e42b754f8760272356fbe9d
Julian Londono <jlond10@gmail.com>
2017-11-10 15:38:01 -0500

    DB fix

--- capital_one_site/settings.py
+++ capital_one_site/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-# Update database configuration with $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
